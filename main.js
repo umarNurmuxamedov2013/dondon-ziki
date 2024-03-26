43fa2dd984e9227e6c6e17e68a5ba809772e7de4
azizqani <nurmuxamedovumar@gmail.com>
2024-03-26 13:31:49 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,67 @@
+let don = prompt('Raqamlardan birontasini tanlang:' + '\n' + '0) qog`oz' + '\n' + '1) qaychi' + '\n' + '2) quduq')
+let random1 = Math.floor(Math.random() * 3)
+
+if (random1 == 0) {
+    alert('Kompyuter qaychini tanladi')
+}
+
+if (random1 == 1) {
+    alert('Kompyuter quduqni tanladi')
+}
+
+if (random1 == 2) {
+    alert('Kompyuter qog`ozni tanladi')
+}
+
+if (don == 0) {
+    if (random1 == 0) {
+        alert('Kompyuter g`alaba qozondi')
+    }else {
+        alert('Siz g`alaba qozondingiz')
+    }
+}
+
+if (don == 1) {
+    if (random1 == 1) {
+        alert('Kompyuter g`alaba qozondi')
+    }else {
+        alert('Siz g`alaba qozondingiz')
+    }
+}
+
+if (don == 2) {
+    if (random1 == 2) {
+        alert('Kompyuter g`alaba qozondi')
+    }else {
+        alert('Siz g`alaba qozondingiz')
+    }
+}
+
+ if (don == 0) {
+     if (random1 == 0) {
+         alert('Kompyuter g`alaba qozondi')
+     } if (don == 0 || random1 == 0) {
+         alert(Durrang)
+     }else {
+         alert('Siz g`alaba qozondingiz')
+     }
+ }
+
+ if (don == 1) {
+     if (random1 == 1) {
+         alert('Kompyuter g`alaba qozondi')
+     } if (don == 1 || random1 == 1) {
+         alert(Durrang)
+     }else {
+         alert('Siz g`alaba qozondingiz')
+     }
+ }
+ if (don == 2) {
+     if (random1 == 2) {
+         alert('Kompyuter g`alaba qozondi')
+     } if (don == 2 || random1 == 2) {
+         alert(Durrang)
+     }else {
+         alert('Siz g`alaba qozondingiz')
+     }
+ }
